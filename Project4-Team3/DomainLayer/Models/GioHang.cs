f5cfd4fb72b72c035cbb27cab38b97c9bd830700
include-daodung <79931452+include-daodung@users.noreply.github.com>
2022-12-29 22:33:54 +0700

    commit

--- Project4-Team3/DomainLayer/Models/GioHang.cs
+++ Project4-Team3/DomainLayer/Models/GioHang.cs
@@ -4,9 +4,9 @@ namespace DomainLayer.Models
 {
     public class GioHang : BaseEntity
     {
-        public string SanPhamId { get; set; }
+        public int SanPhamId { get; set; }
         public int UserId { get; set; }
-        public int SoLuong { get; set; }
+        public String SoLuong { get; set; }
         public decimal? TongTien { get; set; }
         public DateTime? NgayTao { get; set; }
     }
