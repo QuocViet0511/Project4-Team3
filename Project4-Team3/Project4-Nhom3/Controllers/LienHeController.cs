66c653ebfa790ac2964c1dc3300a50e2e2cabf35
include-daodung <79931452+include-daodung@users.noreply.github.com>
2023-02-11 00:29:46 +0700

    m

---
+++ Project4-Team3/Project4-Nhom3/Controllers/LienHeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project4_Nhom3.Controllers
+{
+    public class LienHeController : Controller
+    {
+        public IActionResult Index()
+        {
+             return View("index");
+        }
+    }
+}
